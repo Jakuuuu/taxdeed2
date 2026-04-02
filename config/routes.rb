021a32fdf3faf775fc10e4d839a7e38d53579714
Tax Sale Dev <dev@taxsaleresources.com>
2026-04-02 16:46:15 -0400

    fix: root route - redirect to /users/sign_in (devise/sessions#new not valid outside devise_scope)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-﻿# frozen_string_literal: true
+# frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "devise/sessions#new"
+  root to: redirect("/users/sign_in")
 
   devise_for :users,
     controllers: { registrations: "registrations" }
