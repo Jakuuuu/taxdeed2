08d44d0d440d0ac4e0c73053e867c2ec1279cfb1
Tax Sale Dev <dev@taxsaleresources.com>
2026-04-14 09:39:42 -0400

    fix: bind Puma to 0.0.0.0 for Render port detection

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,9 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Bind to 0.0.0.0 so Render's port scanner can detect the service.
+_port = ENV.fetch("PORT", 3000)
+bind "tcp://0.0.0.0:#{_port}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
